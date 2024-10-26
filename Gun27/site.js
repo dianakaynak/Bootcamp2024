de97fa87616f38b75592ce86207c5acf970ffb21
Diana Kaynak <dianaonder90@gmail.com>
2024-10-26 21:48:35 +0300

    delete and update todo item, json parse, localStorage ödev

--- Gun27/site.js
+++ Gun27/site.js
@@ -1,4 +1,3 @@
-localStorage["diana"] = "dido";
 let todos = [
     {
         title: "Projeyi tamamla",
@@ -15,21 +14,16 @@ let todos = [
     {
         title: "Vitaminlerini al",
         done: true
-    },
-
-
+    }
 ];
 
-//EĞER KAYITLI DATA VARSA ONU
+// EĞER KAYITLI TODOS VARSA ONU
 if (localStorage["data"])
     todos = JSON.parse(localStorage["data"]);
-
-
 
 const divTodos = document.getElementById("todos");
 const frmTask = document.getElementById("frmTask");
 const txtTask = document.getElementById("task");
-
 
 function displayTodos() {
     divTodos.innerHTML = "";
@@ -38,19 +32,36 @@ function displayTodos() {
         const todo = todos[i];
 
         divTodos.innerHTML += `
-      <div class="row mb-3 border border-secondary rounded py-3 px-1 mx-0 bg-primary-subtle">
+            <div class="row mb-3 border border-secondary rounded py-3 px-1 mx-0 bg-primary-subtle ${todo.done ? "done" : "undone"}">
 
                 <div class="col-auto">
-                    <input type="checkbox" class="form-check-input" ${todo.done ? "checked" : ""}>
+                    <input type="checkbox" class="form-check-input" ${todo.done ? "checked" : ""} onchange="toggleItem(${i})">
                 </div>
-                <div class="col ps-0 d-flex align-items-center lead">
-                   ${todo.title}
+                <div class="col ps-0 d-flex align-items-center lead todo-title">
+                    ${todo.title}
+                </div>
+                <div class="col-auto">
+                    <button class="btn btn-danger px-3" onclick="deleteItem(${i})">
+                        <i class="fa-solid fa-xmark"></i>
+                    </button>
                 </div>
 
             </div>
-    `;
+        `;
     }
+}
 
+function toggleItem(index) {
+    const item = todos[index];
+    item.done = !item.done;
+    sortAndSave();
+    displayTodos();
+}
+
+function deleteItem(index) {
+    todos.splice(index, 1);
+    sortAndSave();
+    displayTodos();
 }
 
 function addTask() {
@@ -70,14 +81,10 @@ function sortAndSave() {
     localStorage["data"] = json;
 }
 
-
-//EVENTS
+// EVENTS
 frmTask.onsubmit = function (event) {
     event.preventDefault();
     addTask();
-}
-
+};
 
 displayTodos();
-
-
